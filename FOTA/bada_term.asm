8f62a7b23a21a39a4c3d74706b688a22b364d5ed
bartlomiej.kubica <bartlomiej.kubica@890207be-dda3-6e70-6ae3-c66ba14d969d>
2011-11-13 11:32:08 +0000

    USE_BTERM setting added

--- FOTA/bada_term.asm
+++ FOTA/bada_term.asm
@@ -1,95 +1,13 @@
 include 'inc/settings.inc'		; user dependend settings
 
-CODE_BUFFER equ BUF2_DRAM_START
-DUMP_BUFFER equ CODE_BUFFER + 1 MB
-
 START
+
 	bl	enable_fota_output
-
-	adr	r0, DloadCmdHandler	; register our handler
-	bl	DloadPacketHandler
-
-	mov	r1, #1
-	ldr	r0, [pagetable]
-	bl	MemMMUCacheEnable
-
-	bl	__PfsNandInit
-	bl	__PfsMassInit
-
 	bl	dloadmode
 
 	NORETURN			; endless loop
 
-
-DloadRead:
-	stmfd	sp!, {r4, lr}
-	mov	r3, r1
-	ldrb	r1, [r0, #2]
-	cmp	r3, #1
-	ldrb	ip, [r0, #1]
-	ldrb	r2, [r0, #0]
-	mov	r1, r1, lsl #16
-	ldrb	r4, [r0, #5]
-	orr	r1, r1, ip, lsl #8
-	orr	r1, r1, r2
-	ldrb	r2, [r0, #3]
-	orr	r1, r1, r2, lsl #24
-	ldrb	r2, [r0, #4]
-	orr	r4, r2, r4, lsl #8
-	bne	@f
-	mov	r2, r4
-	ldr	r0, [dump_buf]
-	bl	Flash_Read_Data
-	b	.transmite
-@@:
-	cmp	r3, #2
-	ldmfdne sp!, {r4, pc}
-	ldr	r0, [dump_buf]
-	mov	r2, r4
-	bl	memcpy
-
-.transmite:
-	mov	r1, r4
-	ldr	r0, [dump_buf]
-	ldmfd	sp!, {r4, lr}
-	b	DloadTransmite
-
-
-DloadRunCode:
-	stmfd	sp!, {lr}
-	ldrb	r2, [r0, #0]
-	ldrb	r3, [r0, #1]
-	orr	r2, r2, r3, lsl #8
-	add	r1, r0, #2
-	ldr	r0, [code_buf]
-	bl	memcpy
-	blx	r0
-	ldmfd	sp!, {lr}
-	b	DloadResponseOK
-
-
-DloadCmdHandler:
-	ldrb	r1, [r0]
-	add	r0, #1
-	sub	r3, r1, #1
-	cmp	r3, #1
-	bhi	@f
-	b	DloadRead		; CMD_READ_NAND, CMD_READ_RAM
-@@:
-	cmp	r1, #3
-	bne	@f
-	b	DloadRunCode		; CMD_CODE_RUN
-@@:
-	cmp	r1, #4
-	bxne	lr
-	b	DloadResponseOK 	; CMD_CONN_CHECK
-
-	align 4
-	pagetable			dw gMMUL1PageTable
-	dump_buf			dw DUMP_BUFFER
-	code_buf			dw CODE_BUFFER
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; ==============================================================================
 
 FUNCTIONS
 
