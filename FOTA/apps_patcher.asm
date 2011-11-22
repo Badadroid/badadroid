153f593666fd3d040a49201fbd9a886e85122eeb
dmarszk@gmail.com <dmarszk@gmail.com@890207be-dda3-6e70-6ae3-c66ba14d969d>
2011-11-22 02:42:03 +0000

    Fixed FOTA origin to get branches working. Example modifying S8530XPKJ1 APPS debug output to redirect all messages into WinComm. Fixed example abit.

--- FOTA/apps_patcher.asm
+++ FOTA/apps_patcher.asm
@@ -4,36 +4,42 @@ START
 	stmfd	sp!, {lr}
 
 	ldr	r0, [.src]
-	ldr	r0, [.dst]
+	ldr	r1, [.dst]
 	bl	ARM32_SetBL
 
-	ldmfd	sp!, {pc}               ; back to the bootloader
+	ldmfd	sp!, {pc}		; back to the bootloader
 
 	align 4
-	.src                    dw LaunchNucleus + 0x160    ; NOP NOP NOP sequence in LaunchNucleus
-	.dst                    dw AppsPatcher
+	.src			dw 0x4208984C	 ; NOP NOP NOP sequence in LaunchNucleus
+	.dst			dw AppsPatcher
 
 
 AppsPatcher:
 	stmfd	sp!, {lr}
 
 	; add some code here
-	ldr	r0, [.src]      ; example
-	ldr	r0, [.dst]      ; example
-	bl	ARM32_SetBL     ; example
+       ; ldr     r0, [.src]      ; example
+       ; ldr     r1, [.dst]      ; example
+       ; bl      ARM32_SetBL     ; example
+
+	ldr	r0, [.src]
+	ldr	r1, [MOV_R5_A]
+	str	r1, [r0]
 
 	ldmfd	sp!, {pc}
 
 	align 4
-	.src                    dw 0x40100000
-	.dst                    dw _hook1
+	.src			dw 0x4026DDD4
+       ; .dst                    dw _hook1
+
+MOV_R5_A	dw 0xE3A0500A
 
 
 _hook1:
 	ldr	pc, [loc_4088A2EC]
 
 	align 4
-	loc_4088A2EC            dw 0x4088A2EC
+	loc_4088A2EC		dw 0x4088A2EC
 
 
 ; ==============================================================================
